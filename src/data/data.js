87fb5d4ccc1639ba527f54f0a4a75f58eacb6377
Nipaaaa1 <Frostone224@gmail.com>
2024-01-30 14:07:04 +0700

    Add Data

---
+++ src/data/data.js
@@ -0,0 +1,49 @@
+{
+  navigation: [
+    { title: "About", href: "/" },
+    { title: "Services", href: "/" },
+    { title: "Projects", href: "/" },
+    { title: "Contact", href: "/" },
+  ];
+  hero: {
+    title: "We are creatives";
+  }
+  content: [
+    {
+      title: "Transform your brand",
+      text: "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+    },
+    {
+      title: "Stand out to the right audience",
+      text: "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. ",
+    },
+    {
+      title: "Graphic design",
+      text: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
+    },
+    {
+      title: "Photography",
+      text: "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.",
+    },
+  ];
+  testimonials: [
+    {
+      name: "Emily R.",
+      role: "Marketing Director",
+      testimonial:
+        "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    },
+    {
+      name: "Thomas S.",
+      role: "Chief Operating Officer",
+      testimonial:
+        "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+    },
+    {
+      name: "Jennie F.",
+      role: "Business Owner",
+      testimonial:
+        "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    },
+  ];
+}
